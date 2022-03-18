fb56a95a6ff0b81249933667a68e008bcdb05caa
tylim <limteikyee@gmail.com>
2022-03-18 13:06:27 +0800

    test again

--- src/utilForTests.ts
+++ src/utilForTests.ts
@@ -17,13 +17,13 @@ import { flatten } from './utils'
 export const initializeApp = () => {
 	const env = process.env
 	const config = {
-		apiKey: env.apiKey,
-		authDomain: env.authDomain,
-		projectId: env.projectId,
-		storageBucket: env.storageBucket,
-		messagingSenderId: env.messagingSenderId,
-		appId: env.appId,
-		measurementId: env.measurementId,
+		apiKey: env.API_KEY,
+		authDomain: env.AUTH_DOMAIN,
+		projectId: env.PROJECT_ID,
+		storageBucket: env.STORAGE_BUCKET,
+		messagingSenderId: env.MESSAGING_SENDER_ID,
+		appId: env.APP_ID,
+		measurementId: env.MEASUREMENT_ID,
 	}
 	console.log({ config })
 	return initializeApp_(config)
