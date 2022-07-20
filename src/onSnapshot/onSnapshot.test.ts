fbb0907dba67dc8ad11cb2c6bad9afca94602167
tylim <limteikyee@gmail.com>
2022-07-21 06:44:17 +0800

    1. ignore publish failed 2. each snapshot test delete doc afterward

--- src/onSnapshot/onSnapshot.test.ts
+++ src/onSnapshot/onSnapshot.test.ts
@@ -6,7 +6,7 @@ import {
 	userRefCreator,
 	User,
 } from '../utilForTests'
-import { setDoc } from '../operations'
+import { setDoc, deleteDoc } from '../operations'
 import {
 	IsSame,
 	IsTrue,
@@ -19,14 +19,26 @@ import { where } from '../queryClauses'
 
 initializeApp()
 const userRef = userRefCreator()
+const docId1 = 'onSnapshotOneDocTest'
+const docId2 = 'onSnapshotWithOptionsOneDocTest'
+const docId3 = 'onSnapshotNakedQueryTest'
+const docId4 = 'onSnapshotWithOptionQueryTest'
 describe('test onSnapshot', () => {
+	afterAll(async () => {
+		await Promise.all([
+			deleteDoc(userRef.doc(docId1)),
+			deleteDoc(userRef.doc(docId2)),
+			deleteDoc(userRef.doc(docId3)),
+			deleteDoc(userRef.doc(docId4)),
+		])
+	})
 	it('test isOption', () => {
 		expect(isOptions({})).toBe(false)
 		expect(isOptions({ includeMetadataChanges: false })).toBe(true)
 		expect(isOptions({ includeMetadataChanges: true })).toBe(true)
 	})
 	it('test one doc functionality and type', done => {
-		const docRef = userRef.doc('onSnapshotOneDocTest')
+		const docRef = userRef.doc(docId1)
 		const data = generateRandomData()
 		expect.hasAssertions()
 		setDoc(docRef, data).then(() => {
@@ -36,7 +48,7 @@ describe('test onSnapshot', () => {
 					type A = typeof documentSnapshot
 					type B = DocumentSnapshot<User>
 					IsTrue<IsSame<B, A>>()
-					await compareWriteDataWithDocSnapData(data, documentSnapshot)
+					compareWriteDataWithDocSnapData(data, documentSnapshot)
 					unsub()
 					done()
 				},
@@ -45,7 +57,7 @@ describe('test onSnapshot', () => {
 		})
 	})
 	it('test one doc functionality and type', done => {
-		const docRef = userRef.doc('onSnapshotWithOptionsOneDocTest')
+		const docRef = userRef.doc(docId2)
 		const data = generateRandomData()
 		expect.hasAssertions()
 		setDoc(docRef, data).then(() => {
@@ -55,7 +67,7 @@ describe('test onSnapshot', () => {
 					type A = typeof documentSnapshot
 					type B = DocumentSnapshot<User>
 					IsTrue<IsSame<B, A>>()
-					await compareWriteDataWithDocSnapData(data, documentSnapshot)
+					compareWriteDataWithDocSnapData(data, documentSnapshot)
 					unsub()
 					done()
 				},
@@ -66,8 +78,7 @@ describe('test onSnapshot', () => {
 		})
 	})
 	it('test naked query functionality and type with options', done => {
-		const docId = 'onSnapshotNakedQueryTest'
-		const docRef = userRef.doc(docId)
+		const docRef = userRef.doc(docId3)
 		const data = generateRandomData()
 		expect.hasAssertions()
 		setDoc(docRef, data).then(() => {
@@ -78,14 +89,14 @@ describe('test onSnapshot', () => {
 					type B = QuerySnapshot<User>
 					IsTrue<IsSame<B, A>>()
 					const queryDocumentSnapshot = querySnapshot.docs.filter(
-						doc => doc.id === docId
+						doc => doc.id === docId3
 					)[0]
 					expect(queryDocumentSnapshot).not.toBe(undefined)
 					if (queryDocumentSnapshot) {
 						type C = typeof queryDocumentSnapshot
 						type D = QueryDocumentSnapshot<User>
 						IsTrue<IsSame<C, D>>()
-						await compareWriteDataWithDocSnapData(data, queryDocumentSnapshot)
+						compareWriteDataWithDocSnapData(data, queryDocumentSnapshot)
 					}
 					unsub()
 					done()
@@ -98,8 +109,7 @@ describe('test onSnapshot', () => {
 		})
 	})
 	it('test query with option functionality and type', done => {
-		const docId = 'onSnapshotWithOptionQueryTest'
-		const docRef = userRef.doc(docId)
+		const docRef = userRef.doc(docId4)
 		const data = generateRandomData()
 		expect.hasAssertions()
 		setDoc(docRef, data).then(() => {
@@ -110,7 +120,7 @@ describe('test onSnapshot', () => {
 					type B = QuerySnapshot<User>
 					IsTrue<IsSame<B, A>>()
 					const queryDocumentSnapshot = querySnapshot.docs.filter(
-						doc => doc.id === docId
+						doc => doc.id === docId4
 					)[0]
 					expect(querySnapshot.docs.length).toBe(1)
 					expect(queryDocumentSnapshot).not.toBe(undefined)
@@ -118,7 +128,7 @@ describe('test onSnapshot', () => {
 						type C = typeof queryDocumentSnapshot
 						type D = QueryDocumentSnapshot<User>
 						IsTrue<IsSame<C, D>>()
-						await compareWriteDataWithDocSnapData(data, queryDocumentSnapshot)
+						compareWriteDataWithDocSnapData(data, queryDocumentSnapshot)
 					}
 					unsub()
 					done()
