a9eb11e03f355d9587fba68c1321bf8cc06b3c3d
tylim <limteikyee@gmail.com>
2022-08-05 06:19:47 +0800

    simplify increment tests

--- src/fieldValue/increment.test.ts
+++ src/fieldValue/increment.test.ts
@@ -13,21 +13,15 @@ describe('test increment', () => {
 	const docRef = ref.doc('increment')
 	it('test with set', async () => {
 		await setDoc(docRef, { a: -100 })
-		await setDoc(docRef, { a: increment(100) })
+		await setDoc(docRef, { a: increment(100) }) // set increment does not update
 		const docSnap = await getDoc(docRef)
 		const data = docSnap.data()
-		expect(data).not.toBe(undefined)
-		if (data) {
-			expect(data.a).toBe(100)
-		}
+		expect(data?.a).toBe(100)
 	})
 	it('test with update ', async () => {
 		await updateDoc(docRef, { a: increment(-100) })
-		const docSnap2 = await getDoc(docRef)
-		const data2 = docSnap2.data()
-		expect(data2).not.toBe(undefined)
-		if (data2) {
-			expect(data2.a).toBe(0)
-		}
+		const docSnap = await getDoc(docRef)
+		const data = docSnap.data()
+		expect(data?.a).toBe(0)
 	})
 })
