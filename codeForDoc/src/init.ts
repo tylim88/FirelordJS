385caff5d87510bfb09fc092d12acfbac324c738
Acid Coder <5227509+tylim88@users.noreply.github.com>
2022-09-04 05:09:10 +0800

    Update init.ts

--- codeForDoc/src/init.ts
+++ codeForDoc/src/init.ts
@@ -2,16 +2,16 @@ import { getFirelord, getFirestore } from 'firelordjs'
 import { initializeApp } from 'firebase/app'
 import { Example } from './dataType'
 
-initializeApp({
-	apiKey: '### FIREBASE API KEY ###',
-	authDomain: '### FIREBASE AUTH DOMAIN ###',
-	projectId: '### CLOUD FIRESTORE PROJECT ID ###',
+const app = initializeApp({
+	projectId: '### PROJECT ID ###',
 })
 
-export const db = getFirestore()
+export const db = getFirestore() // or getFirestore(app), you can skip this line
 
-const firelordExample = getFirelord<Example>(db)
-// OR
-const firelordExample_Alt = getFirelord<Example>()
+// Recommendation: Export this if the collection is sub-collection and then fill in collection path later, because sub collection most likely have dynamic document ID.
+export const firelordExample = getFirelord<Example>() // or getFirelord<Example>(db)
+// then call them using
+// const example = firelordExample(`parentCollectionName/${someDocId}/SomeCollectionName`) // this is your firelordRef
 
-export const example = firelordExample('SomeCollectionName')
+// Recommendation: Export this if the collection is root collection
+export const example = firelordExample('SomeCollectionName') // this is your firelordRef
