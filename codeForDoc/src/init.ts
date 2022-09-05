0bcfaa06528365bbb3ffc79bb76512505c7b4625
tylim <limteikyee@gmail.com>
2022-09-05 10:26:32 +0800

    housekeeping

--- codeForDoc/src/init.ts
+++ codeForDoc/src/init.ts
@@ -6,12 +6,8 @@ const app = initializeApp({
 	projectId: '### PROJECT ID ###',
 })
 
-export const db = getFirestore() // or getFirestore(app), you can skip this line
+export const db = getFirestore()
 
-// Recommendation: Export this if the collection is sub-collection and then fill in collection path later, because sub collection most likely have dynamic document ID.
-export const firelordExample = getFirelord<Example>() // or getFirelord<Example>(db)
-// then call them using
-// const example = firelordExample(`parentCollectionName/${someDocId}/SomeCollectionName`) // this is your firelordRef
+export const firelordExample = getFirelord<Example>()
 
-// Recommendation: Export this if the collection is root collection
-export const example = firelordExample('SomeCollectionName') // this is your firelordRef
+export const example = firelordExample('SomeCollectionName')
