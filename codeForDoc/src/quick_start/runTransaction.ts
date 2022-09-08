51fa0066f568616e9613f0e6ef8691848878d536
tylim <limteikyee@gmail.com>
2022-09-08 21:13:35 +0800

    update code for doc

--- codeForDoc/src/quick_start/runTransaction.ts
+++ codeForDoc/src/quick_start/runTransaction.ts
@@ -12,25 +12,28 @@ export const dummy = async () => {
 			// ...
 		})
 		// OR you can skip 'db'
-		await runTransaction(async transaction => {
-			await transaction.get(example.doc('lmn')).then(docSnapshot => {
-				const data = docSnapshot.data()
-			})
+		await runTransaction(
+			async transaction => {
+				await transaction.get(example.doc('lmn')).then(docSnapshot => {
+					const data = docSnapshot.data()
+				})
 
-			transaction.set(example.doc('lmn'), {
-				a: 88,
-				b: { c: false, d: [{ e: 'opq' }] },
-				f: { g: serverTimestamp(), h: 2929 },
-			})
+				transaction.set(example.doc('lmn'), {
+					a: 88,
+					b: { c: false, d: [{ e: 'opq' }] },
+					f: { g: serverTimestamp(), h: 2929 },
+				})
 
-			transaction.update(example.doc('lmn'), {
-				a: increment(1),
-				b: { d: arrayRemove({ e: 'rst' }) }, // nested form
-				'f.g': serverTimestamp(), // dot notation form
-			})
+				transaction.update(example.doc('lmn'), {
+					a: increment(1),
+					b: { d: arrayRemove({ e: 'rst' }) }, // nested form
+					'f.g': serverTimestamp(), // dot notation form
+				})
 
-			transaction.delete(example.doc('lmn'))
-		})
+				transaction.delete(example.doc('lmn'))
+			},
+			{ maxAttempts: 10 }
+		)
 		console.log('Transaction successfully committed!')
 	} catch (e) {
 		console.log('Transaction failed: ', e)
