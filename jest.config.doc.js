bde8d0a5eb4d08936544e9b2029a7af3ac262f4a
tylim <limteikyee@gmail.com>
2023-02-13 07:32:59 +0800

    remove jest

--- jest.config.doc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const sharedConfig = require('./jest.config.js')
-module.exports = {
-	...sharedConfig,
-	roots: ['<rootDir>/codeForDoc'],
-	collectCoverage: false,
-}
