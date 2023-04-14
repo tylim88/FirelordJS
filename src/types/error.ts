3020a01ae9c617d4efa25d2c854c075fcb9ea9c2
tylim <limteikyee@gmail.com>
2023-04-14 19:27:34 +0800

    fix `where` clause return `never` error upon incorrect field path type

--- src/types/error.ts
+++ src/types/error.ts
@@ -33,7 +33,9 @@ export type ErrorEmptyDocumentOrCollectionID<
 export type ErrorNumberOfForwardSlashIsNotEqual<
 	Correct extends number,
 	Current extends number
-> = `Error: Invalid query, forward slash count mismatched, current count is ${Current}, need ${Correct}.`
+> = `Error: ${Current extends 0
+	? `You need to assert your value for documentId() as const, eg: ''a/b/c' as const' or else the forward slash count would be 0`
+	: `Invalid query, forward slash count mismatched`}, current count is ${Current}, need ${Correct}.`
 export type ErrorEmptyUpdate = `Error: Update data is an empty object literal`
 export type ErrorPossiblyUndefinedAsArrayElement =
 	`Error: You cannot assign PossiblyUndefined as array element, eg: 'PossiblyUndefined[]', you can however assign PossiblyUndefined anyway you want to array element, eg: < { a : number | PossiblyUndefined }[] >`
