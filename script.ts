0628fd4f5c6ddb08f7d217b7c814e5ee7f0df9ed
tylim <limteikyee@gmail.com>
2023-06-17 00:54:16 +0800

    improve build cjs script

--- script.ts
+++
@@ -1,8 +0,0 @@
-import pkg from './package.json'
-import fs from 'fs'
-
-pkg.version = pkg.version + '-cjs'
-
-const content = JSON.stringify(pkg)
-
-fs.writeFileSync('package.json', content, 'utf8')
